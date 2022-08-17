c13ff93a38a6565d9fa01b90ba9fbb8c4a08c2c2
Akhil Jayaprakash <akhiljayaprakash@users-MacBook-Pro-41.local>
2022-08-17 14:28:54 +0530

    reverting back to main

--- orders/main.go
+++ orders/main.go
@@ -8,7 +8,7 @@ import (
 
 func hello(w http.ResponseWriter, _ *http.Request) {
 
-	_, err := fmt.Fprintf(w, "Akhil Hello Orders %d \n", utils.Trigger())
+	_, err := fmt.Fprintf(w, "Hello Orders %d \n", utils.Trigger())
 	if err != nil {
 		panic(err)
 	}
